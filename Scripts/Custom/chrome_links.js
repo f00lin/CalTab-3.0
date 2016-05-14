cbdd37106f2599401939fc9553e052fabe2a5539
James Thomas <dairy_water@yahoo.co.uk>
2016-05-14 07:28:08 +0100

    updated extension ID

--- Scripts/Custom/chrome_links.js
+++ Scripts/Custom/chrome_links.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('options').addEventListener('click', function() {
-        chrome.tabs.update({ url: 'chrome://extensions?options=fkenfnefflongfibiaibcgbmjoeehmdi' });
+        chrome.tabs.update({ url: 'chrome://extensions?options=glbhmgdfmbgkdehpoegiafnkiibeklmn' });
     });
 });
 
